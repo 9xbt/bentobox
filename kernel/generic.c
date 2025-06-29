8a1fc8632dfd59312ccb3ed417fead8272c4c9af
xrc2 <109512837+9xbt@users.noreply.github.com>
2025-06-29 20:15:19 +0200

    generic: cleanup test task

--- kernel/generic.c
+++ kernel/generic.c
@@ -6,16 +6,6 @@
 #include <kernel/printf.h>
 #include <kernel/version.h>
 
-void time_process(void) {
-    size_t uptime = 0;
-    for (;;) {
-        printf("\rUptime: %lu seconds", uptime++);
-        sched_sleep(1000000);
-        printf("Bye bye!\n");
-        sched_kill(this, 0);
-    }
-}
-
 void generic_startup(void) {
     vfs_install();
 	pci_scan();
@@ -26,7 +16,5 @@ void generic_startup(void) {
 void generic_main(void) {
     dprintf("%s:%d: running init process\n", __FILE__, __LINE__);
     spawn("/bin/init", 0, NULL, NULL);
-
-    //sched_add_task(sched_new_task(time_process, "Uptime"), NULL);
 	sched_jumpstart();
 }
