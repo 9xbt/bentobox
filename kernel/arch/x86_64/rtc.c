6c344af8d56018088cbb2e2b5be76013202e108f
9xbt <flippy632@gmail.com>
2025-07-13 23:28:28 +0200

    ports: fix doomgeneric

--- kernel/arch/x86_64/rtc.c
+++ kernel/arch/x86_64/rtc.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <kernel/arch/x86_64/io.h>
+#include <kernel/arch/x86_64/tsc.h>
+#include <kernel/arch/x86_64/hpet.h>
 
 #define from_bcd(value) ((value >> 4) * 10 + (value & 0xf))
 #define is_leap_year(year) (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
@@ -50,4 +52,8 @@ uint64_t now(void) {
 
 void gettimeofday(long *sec, long *nsec) {
     if (sec) *sec = now();
+    if (nsec) {
+        if (hpet) hpet_read_time(NULL, nsec);
+        else tsc_read_time(NULL, nsec);
+    }
 }
