ce7d68ee8c501361506808d8a70e4f55ed9f2661
9xbt <flippy632@gmail.com>
2025-07-24 01:03:35 +0200

    modules/ext2: singly & doubly blocks

--- modules/ext2.c
+++ modules/ext2.c
@@ -122,14 +122,6 @@ void ext2_read_block(ext2_fs *fs, uint32_t block, void *buffer, uint32_t count) 
     vfs_read(fs->sda, buffer, block * fs->block_size, count);
 }
 
-void ext2_read_blocks(ext2_fs *fs, uint32_t *blocks, void *buffer, uint32_t count) {
-    for (uint32_t i = 0; i < count; i++) {
-        if (blocks[i]) {
-            ext2_read_block(fs, blocks[i], (uint8_t*)buffer + (i * fs->block_size), fs->block_size);
-        }
-    }
-}
-
 void ext2_read_inode(ext2_fs *fs, uint32_t inode, ext2_inode *in) {
     inode--;
     uint32_t block_group = inode / fs->sb->inodes_per_group;
@@ -140,28 +132,99 @@ void ext2_read_inode(ext2_fs *fs, uint32_t inode, ext2_inode *in) {
     ext2_read_block(fs, fs->bgd_table[block_group].inode_table_block + inode_block, buffer, fs->block_size);
     memcpy(in, buffer + (inode_index % (fs->block_size / fs->inode_size)) * fs->inode_size, fs->inode_size);
 }
-                                                                                // start block     block count
+
+void ext2_read_direct_blocks(ext2_fs *fs, uint32_t *blocks, void *buffer, uint32_t count) {
+    for (uint32_t i = 0; i < count; i++) {
+        if (blocks[i]) {
+            ext2_read_block(fs, blocks[i], (uint8_t*)buffer + (i * fs->block_size), fs->block_size);
+        }
+    }
+}
+
+void ext2_read_singly_blocks(ext2_fs *fs, uint32_t indirect_block, uint8_t *buffer, uint32_t offset, uint32_t count) {
+    if (!indirect_block)
+        return;
+
+    uint32_t singly_ptr = fs->block_size / sizeof(uint32_t);
+    if (offset >= singly_ptr)
+        return;
+    if (offset + count > singly_ptr)
+        count = singly_ptr - offset;
+
+    uint32_t *block_ptrs = kmalloc(fs->block_size);
+    ext2_read_block(fs, indirect_block, block_ptrs, fs->block_size);
+
+    for (uint32_t i = 0; i < count; i++) {
+        if (block_ptrs[offset + i]) {
+            ext2_read_block(fs, block_ptrs[offset + i], buffer + (i * fs->block_size), fs->block_size);
+        }
+    }
+
+    kfree(block_ptrs);
+}
+
+void ext2_read_doubly_blocks(ext2_fs *fs, uint32_t doubly_block, uint8_t *buffer, uint32_t offset, uint32_t count) {
+    if (!doubly_block)
+        return;
+
+    uint32_t ptrs_per_block = fs->block_size / sizeof(uint32_t);
+    if (offset >= ptrs_per_block)
+        return;
+    if (offset + count > ptrs_per_block)
+        count = ptrs_per_block - offset;
+
+    uint32_t *doubly_ptrs = kmalloc(fs->block_size);
+    ext2_read_block(fs, doubly_block, doubly_ptrs, fs->block_size);
+
+    for (uint32_t i = 0, read = 0; i < count; i++) {
+        if (doubly_ptrs[offset + i]) {
+            ext2_read_singly_blocks(fs, doubly_ptrs[offset + i], buffer + read * fs->block_size, 0, ptrs_per_block);
+            read += ptrs_per_block;
+        }
+    }
+
+    kfree(doubly_ptrs);
+}
+
 void ext2_read_inode_blocks(ext2_fs *fs, ext2_inode *in, uint8_t *buffer, uint32_t block, uint32_t block_count) {
+    uint32_t blocks_per_singly = fs->block_size / 4;
+    uint32_t blocks_per_doubly = blocks_per_singly * blocks_per_singly;
     uint32_t current = block;
     uint32_t remaining = block_count;
     uint32_t offset = 0;
     
     if (current < 12 && remaining > 0) {
-        uint32_t count = (remaining < 12 - current) ? remaining : (12 - current);
-        ext2_read_blocks(fs, &in->direct_block_ptr[current], buffer + offset, count);
+        uint32_t count = remaining < 12 - current ? remaining : 12 - current;
+        ext2_read_direct_blocks(fs, &in->direct_block_ptr[current], buffer + offset, count);
 
         offset += count * fs->block_size;
         current += count;
         remaining -= count;
     }
+
+    if (current < 12 + blocks_per_singly && remaining > 0 && in->singly_block_ptr != 0) {
+        uint32_t count = current + remaining > 12 + blocks_per_singly ? 12 + blocks_per_singly - current : remaining;
+        ext2_read_singly_blocks(fs, in->singly_block_ptr, buffer + offset,
+            current >= 12 ? current - 12 : 0 * fs->block_size, count * fs->block_size);
+
+        offset += count * fs->block_size;
+        current += count;
+        remaining -= count;
+    }
+
+    if (current < 12 + blocks_per_singly + blocks_per_doubly && remaining > 0 && in->doubly_block_ptr != 0) {
+        ext2_read_doubly_blocks(fs, in->doubly_block_ptr, buffer + offset,
+            ((current >= 12 + blocks_per_singly) ? current - (12 +
+            blocks_per_singly) : 0) * fs->block_size, remaining * fs->block_size);
+    }
 }
 
 long ext2_read(struct vfs_node *node, void *buffer, long offset, size_t len) {
     ext2_fs *fs = node->device;
-    if (!fs || fs->sb->signature != 0xef53) {
-        dprintf("%s:%d: node has ext2 device, but superblock is invalid?\n", __FILE__, __LINE__);
+    if (!fs)
+        return -ENOENT;
+    if (fs->sb->signature != 0xef53)
         return -EIO;
-    }
     
     ext2_inode inode;
     ext2_read_inode(fs, node->inode, &inode);
@@ -204,7 +267,8 @@ void ext2_mount_directory(ext2_fs *fs, uint8_t *block_data, size_t block_size, s
 
     while (offset < block_size) {
         ext2_dirent *entry = (ext2_dirent *)(block_data + offset);
-        if (entry->inode == 0 || entry->total_size == 0) break;
+        if (entry->inode == 0 || entry->total_size == 0)
+            break;
 
         char name[entry->name_len + 1];
         memcpy(name, entry->name, entry->name_len);
@@ -240,7 +304,8 @@ void ext2_mount(ext2_fs *fs, struct vfs_node *parent, uint32_t in) {
     uint32_t total_blocks = (inode.size + fs->block_size - 1) / fs->block_size;
 
     for (uint32_t i = 0; i < total_blocks && i < 12; i++) {
-        if (inode.direct_block_ptr[i] == 0) continue;
+        if (inode.direct_block_ptr[i] == 0)
+            continue;
 
         uint8_t *block = kmalloc(fs->block_size);
         ext2_read_block(fs, inode.direct_block_ptr[i], block, fs->block_size);
