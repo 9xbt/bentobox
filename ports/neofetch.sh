a0c6c5c21d1147413177df6bbe76cce21eb95cdd
9xbt <flippy632@gmail.com>
2025-09-08 20:02:29 +0200

    ports/neofetch: make it executable

--- ports/neofetch.sh
+++ ports/neofetch.sh
@@ -2,3 +2,4 @@
 mkdir -p root/usr/bin
 curl -L -o root/usr/bin/neofetch https://raw.githubusercontent.com/dylanaraps/neofetch/refs/heads/master/neofetch
 patch root/usr/bin/neofetch < ports/neofetch.patch
+chmod +x root/usr/bin/neofetch
