c0fde7ce5c9dac75e9c036e94b4b3e7bdfe23dab
xrc2 <109512837+9xbt@users.noreply.github.com>
2025-10-04 12:19:44 +0200

    ports: lua

--- base/root/.bashrc
+++ base/root/.bashrc
@@ -3,3 +3,4 @@
 #
 
 alias ls='ls --color=auto'
+alias clear='echo -en "\033[2J\033[H"'
---
+++ ports/lua.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+WD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+MLIBC="$WD/../build/mlibc/x86_64"
+
+mkdir -p ports/src
+cd ports/src
+git clone https://github.com/lua/lua
+cd lua
+git apply ../../lua.diff
+make CC=musl-gcc MYCFLAGS="-I$MLIBC/include" MYLDFLAGS="-L$MLIBC/lib -nostdlib -static $MLIBC/lib/crt0.o" MYLIBS="-Wl,--start-group -lc -lgcc -lgcc_eh -Wl,--end-group" -j$(nproc)
+cp lua ../../../base/usr/bin
