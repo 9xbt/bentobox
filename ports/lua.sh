5ecbcd58e5c97765d77be0f5cde4114c34f721b8
xrc2 <109512837+9xbt@users.noreply.github.com>
2025-10-04 14:02:20 +0200

    ports/lua: fix lua.sh

--- ports/lua.sh
+++ ports/lua.sh
@@ -7,5 +7,5 @@ cd ports/src
 git clone https://github.com/lua/lua
 cd lua
 git apply ../../lua.diff
-make CC=musl-gcc MYCFLAGS="-I$MLIBC/include" MYLDFLAGS="-L$MLIBC/lib -nostdlib -static $MLIBC/lib/crt0.o" MYLIBS="-Wl,--start-group -lc -lgcc -lgcc_eh -Wl,--end-group" -j$(nproc)
+make CC=gcc MYCFLAGS="-I$MLIBC/include" MYLDFLAGS="-L$MLIBC/lib -nostdlib -static $MLIBC/lib/crt0.o" MYLIBS="-Wl,--start-group -lc -lgcc -lgcc_eh -Wl,--end-group" -j$(nproc)
 cp lua ../../../base/usr/bin
