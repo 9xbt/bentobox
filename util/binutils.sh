8e101e4e136940860a6ef6af61fc2538c2ff3188
9xbt <flippy632@gmail.com>
2025-11-30 22:32:25 +0100

    bentobox: GCC cross compiler

---
+++ util/binutils.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ARCH=x86_64
+TARGET=x86_64-pc-bentobox
+PREFIX="$CWD/build"
+SYSROOT="$PREFIX/$TARGET"
+
+set -e
+mkdir -p $CWD/../build/binutils
+cd $CWD/../build/binutils
+CC="ccache gcc" CXX="ccache g++" ../../util/binutils-gdb/configure --target=$TARGET --prefix=$PREFIX --with-sysroot --disable-nls --disable-werror
+make -j$(nproc)
+make install
