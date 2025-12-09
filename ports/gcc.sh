27b6de69e63bfe512f9aadc1ca45ff8b6f36a3c0
9xbt <flippy632@gmail.com>
2025-12-09 22:33:08 +0100

    gcc: actually push the patches

--- ports/gcc.sh
+++ ports/gcc.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 [ -z "$BASE" ] && echo "Please run . build/mlibc-root before building GCC!" && exit 1
 
-CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../util
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PREFIX="$BASE/usr"
 export PATH="$CWD/build/bin:$PATH"
 
 set -e
-cd $CWD/gcc
+cd $CWD/../util/gcc
 ./contrib/download_prerequisites
+cd gettext/build-aux
+patch -p0 < $CWD/gettext.diff
 cd -
+cd gmp
+patch -p0 < $CWD/gmp.diff
+cd -
+cd isl
+patch -p0 < $CWD/isl.diff
+cd -
+cd mpfr
+patch -p0 < $CWD/mpfr.diff
 
 mkdir -p /tmp/gcc-stage3
 cd /tmp/gcc-stage3
-CC="ccache $TARGET-gcc" CXX="ccache $TARGET-g++" $CWD/gcc/configure --host=$TARGET --target=$TARGET --prefix=$PREFIX --with-sysroot=/ --with-build-sysroot=$BASE --libexecdir="$PREFIX/lib" --disable-nls --disable-multilib --enable-languages=c,c++ --disable-libssp --disable-libgomp --disable-libquadmath --disable-lto-plugin --disable-default-pie --disable-shared --enable-static
+CC="ccache $TARGET-gcc" CXX="ccache $TARGET-g++" $CWD/../util/gcc/configure --host=$TARGET --target=$TARGET --prefix=$PREFIX --with-sysroot=/ --with-build-sysroot=$BASE --libexecdir="$PREFIX/lib" --disable-nls --disable-multilib --enable-languages=c,c++ --disable-libssp --disable-libgomp --disable-libquadmath --disable-lto-plugin --disable-default-pie --disable-shared --enable-static
 make all-gcc -j$(nproc)
 make all-target-libgcc -j$(nproc)
 # make all-target-libstdc++-v3 -j$(nproc)
