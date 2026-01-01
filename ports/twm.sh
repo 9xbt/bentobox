970c86c6ec653cc587374e9d348dee3f497490d1
9xbt <flippy632@gmail.com>
2026-01-01 18:09:14 +0100

    ports: twm & xeyes

---
+++ ports/twm.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+[ -z "$MLIBC_ROOT" ] || [ -z "$BASE" ] && echo "Please run .  build/mlibc-root before building ports!" && exit 1
+
+export CC="${TOOLCHAIN_PREFIX:-}gcc"
+export CXX="${TOOLCHAIN_PREFIX:-}g++"
+export LD="${TOOLCHAIN_PREFIX:-}ld"
+export CFLAGS="-g -O2"
+export CXXFLAGS="-g -O2"
+
+export CC_FOR_BUILD="gcc"
+export CXX_FOR_BUILD="g++"
+export CFLAGS_FOR_BUILD="-O2"
+export LDFLAGS_FOR_BUILD=""
+
+export PKG_CONFIG_PATH="$BASE/usr/lib/pkgconfig:$BASE/usr/share/pkgconfig"
+export PKG_CONFIG_LIBDIR="$BASE/usr/lib/pkgconfig: $BASE/usr/share/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$BASE"
+export ACLOCAL_PATH="$BASE/usr/share/aclocal"
+
+mkdir -p ports/src
+git clone https://gitlab.freedesktop.org/xorg/app/twm.git ports/src/twm --depth=1
+cd ports/src/twm
+
+make distclean 2>/dev/null || true
+
+set -e
+autoreconf -fvi
+cp ../../config.sub . 
+./configure --host=$ARCH-pc-bentobox --prefix=/usr --enable-shared --disable-static
+
+make -j"$(nproc)"
+make DESTDIR=$BASE install
