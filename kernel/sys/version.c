b43c9facc8b6e26a286db6989e62d2c931d9a5ea
9xbt <flippy632@gmail.com>
2026-01-04 16:53:02 +0100

    bentobox: bump version

--- kernel/sys/version.c
+++ kernel/sys/version.c
@@ -1,7 +1,7 @@
 const char *__kernel_name = "bentobox";
 
 int __kernel_version_major = 0;
-int __kernel_version_minor = 3;
+int __kernel_version_minor = 4;
 int __kernel_version_patch = 0;
 
 const char *__kernel_build_date = __DATE__;
